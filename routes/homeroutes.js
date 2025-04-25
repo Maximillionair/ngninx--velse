b86e1795f3809936133fd77c856e5c22d3008b92
Maximillionair <maxmillian.heim@outlook.com>
2025-04-25 11:17:56 +0200

    Update homeroutes.js

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../app/controllers/homeController');
+const homeController = require('../controllers/homecontroller');
 
 router.get('/', homeController.getIndex);
 
